d1a0e974dedcb1b4efcfc8646ad13281c04eeb5d
Akilan Manogaran <72593032+akilan1700@users.noreply.github.com>
2026-06-08 16:01:43 +0530

    Fix potential XSS vulnerability in search endpoint
    Updated response to send a static string instead of user input.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/search', (req, res) => {
-  res.send(`<h1>${req.query.q}</h1>`); // Vulnerable
+  res.send(`<h1>query</h1>`); // Vulnerable
 });
 
 app.listen(3001, () => console.log("Running on 3001"));
